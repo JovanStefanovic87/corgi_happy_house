d50518acdf53cd5e731862489fccdac6b48e2760
Jovan Stefanovic <33285650+JovanStefanovic87@users.noreply.github.com>
2022-07-06 20:56:40 +0200

    Add files via upload

---
+++ languages/index.js
@@ -0,0 +1,11 @@
+import en from './en.json';
+import de from './de.json';
+import rs from './rs.json';
+
+export const dictionaryList = { en, de, rs };
+
+export const languageOptions = {
+  en: 'English',
+  de: 'Deutsch',
+  rs: 'Srpski'
+};
